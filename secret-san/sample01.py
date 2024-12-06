4d56da877f54c622ba39b7ddf7b7f25cefba3210
tsato1222 <39904312+tsato1222@users.noreply.github.com>
2024-12-06 17:37:28 +0900

    Create sample01.py

---
+++ secret-san/sample01.py
@@ -0,0 +1,17 @@
+import boto3
+
+aws_access_key = 'AKIAIOSFODNN7EXAMPLE'
+aws_secret_key = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYzEXAMPLEKEY'
+
+session = boto3.Session(
+    aws_access_key_id=aws_access_key,
+    aws_secret_access_key=aws_secret_key,
+    region_name='us-west-2'
+)
+
+s3 = session.client('s3')
+
+response = s3.list_buckets()
+
+for bucket in response['Buckets']:
+    print(f'- {bucket["Name"]}')
