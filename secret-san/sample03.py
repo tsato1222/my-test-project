bd0cf34304decb8733c42a8a70bb73d1064eb593
tomoki.sato.jf <tomoki.sato.jf@hitachi-solutions.com>
2024-12-09 14:57:22 +0900

    test

--- secret-san/sample03.py
+++ secret-san/sample03.py
@@ -1,6 +1,6 @@
 # GitHub Personal Access Token (PAT)
 # WARNING: Do NOT include sensitive keys in your code like this in production!
-GITHUB_TOKEN = 'ghp_1234567890abcdef1234567890abcdef12345678'
+GITHUB_TOKEN = 'ghp_abcDexF0lsop3kKiqPl2rEo03R2qIo0495oe'
 
 # Another example of secret: API Key for a third-party service
 API_KEY = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
